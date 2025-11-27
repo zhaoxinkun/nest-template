04ec0305621502669106dcc677d5de454905b558
zhaoxinkun <zhaoxinkun1124@gmail.com>
2025-11-27 23:11:51 +0800

    Revert "dotenv"
    This reverts commit 38aae49171e60c056a577324edb0ef4a776e7f62.

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import process from 'process';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    // 获取env数据
-    const port = process.env.MYSQL_PORT ?? 3000;
-    return `Hello World! Port: ${port}`;
+    return 'Hello World!';
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// 使用dotenv
-import * as dotenv from 'dotenv';
-
-dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
-
 bootstrap();
