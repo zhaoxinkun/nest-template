38aae49171e60c056a577324edb0ef4a776e7f62
zhaoxinkun <zhaoxinkun1124@gmail.com>
2025-11-27 15:01:19 +0800

    dotenv

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import process from 'process';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    // 获取env数据
+    const port = process.env.MYSQL_PORT ?? 3000;
+    return `Hello World! Port: ${port}`;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// 使用dotenv
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
