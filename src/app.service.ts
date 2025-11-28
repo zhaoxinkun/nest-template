b5d6c2e0a8e4a92391c3dce4bb8034d782c0b05f
zhaoxinkun <zhaoxinkun1124@gmail.com>
2025-11-28 11:50:58 +0800

    Revert "Reapply "config for read .json""
    This reverts commit f0b1915b607ff28d7d23f26b4ad3abd78f631ef6.

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import config from 'config';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    const dbPort = config.get('Customer.dbConfig.port');
-    return `Hello World! DB Port: ${dbPort}`;
+    return 'Hello World!';
   }
 }
