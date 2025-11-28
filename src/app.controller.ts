52fb319b32193b31d10ec52527846b61ff75dc84
zhaoxinkun <zhaoxinkun1124@gmail.com>
2025-11-28 12:46:05 +0800

    nest-logger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  // 导入内置的log
+  private readonly logger = new Logger(AppController.name);
+
+  constructor(private readonly appService: AppService) {
+  }
 
   @Get()
   getHello(): string {
+    // 使用内置的log
+    this.logger.warn('getHello() is called');
     return this.appService.getHello();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MyLogger } from './logConfig/MyLogger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    // logger: new MyLogger(),
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
