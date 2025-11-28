97d07049fea6ddcde346e7f7b4b3e8c3ebf951ca
zhaoxinkun <zhaoxinkun1124@gmail.com>
2025-11-28 13:25:56 +0800

    pino pino-pretty pino-roll

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Logger } from 'nestjs-pino';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService,
+              private readonly logger: Logger) {
+  }
 
   @Get()
   getHello(): string {
+    this.logger.warn('getHello() is called');
     return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,41 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [],
+  imports: [
+    LoggerModule.forRoot({
+      pinoHttp: {
+        autoLogging: true,
+        transport: {
+          targets: [
+            {
+              // 配置格式化
+              target: 'pino-pretty',
+              options: {
+                colorize: true,
+              },
+            },
+            {
+              // 配置滚动输出
+              target: 'pino-roll',
+              options: {
+                mkdir: true,              // 自动创建日志目录
+                file: './logs/app.log',   // 必须指定文件路径
+                interval: '1d',           // 每天滚动
+                maxFiles: 7,              // 保留最近 7 个日志文件
+                colorize: true,          // 文件里不需要彩色
+              },
+            },
+          ],
+        },
+
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
