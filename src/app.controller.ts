44b99b93f784f9fc1f1b46e3a5cd2cbf2f642fc6
zhaoxinkun <zhaoxinkun1124@gmail.com>
2025-11-28 17:04:50 +0800

    Revert "pino pino-pretty pino-roll"
    This reverts commit 97d07049fea6ddcde346e7f7b4b3e8c3ebf951ca.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Logger } from 'nestjs-pino';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService,
-              private readonly logger: Logger) {
-  }
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
-    this.logger.warn('getHello() is called');
     return this.appService.getHello();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { bufferLogs: true });
-  app.useLogger(app.get(Logger));
+  const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
-
 bootstrap();
