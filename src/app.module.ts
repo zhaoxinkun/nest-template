7e5459b0d6c665a0c64de9fda9ba2914454b9b4e
zhaoxinkun <zhaoxinkun1124@gmail.com>
2025-11-28 12:00:29 +0800

    Revert "@nestjs/config"
    This reverts commit c16204023e73a914299f299864537ac34a31a542.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-      isGlobal: true,
-    }),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {
-}
+export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  constructor(private configService: ConfigService) {
-  }
-
   getHello(): string {
-    const dbPort = this.configService.get('DB_TYPE');
-    return `Hello World! DB Port: ${dbPort}`;
+    return 'Hello World!';
   }
 }
